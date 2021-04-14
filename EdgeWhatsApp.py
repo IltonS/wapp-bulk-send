553b1b5daf539485c188659a60daeaa66833499b
IltonS <ilton.sequeira.s@gmail.com>
2021-04-14 17:01:43 -0300

    Adiciona a classe EdgeWhatsApp para enviar mensagens

---
+++ EdgeWhatsApp.py
@@ -0,0 +1,28 @@
+﻿from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+import sys
+
+class EdgeWhatsApp:   
+   
+   def __init__(self,driver_path):
+      # Set drive
+      self.driver = webdriver.Edge(executable_path=driver_path)
+      self.driver.get("https://web.whatsapp.com/")
+      self.wait = WebDriverWait(self.driver, 600)
+
+   def send_msg(self, to, msg):
+      self.quoted_to = '"' + to + '"'
+      x_arg = '//span[contains(@title,' + self.quoted_to + ')]'
+      group_title = self.wait.until(EC.presence_of_element_located((By.XPATH, x_arg)))
+      group_title.click()
+      message = self.driver.find_elements_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')[0]
+      message.send_keys(msg)
+      sendbutton = self.driver.find_elements_by_xpath('//*[@id="main"]/footer/div[1]/div[3]/button')[0]
+      sendbutton.click()
+      
+   def __del__(self):
+      self.driver.close() 
--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-﻿def main():
-   print('wapp-bulk-send')
+﻿# Referências:
+#    https://www.tutorialspoint.com/whatsapp-using-python
+#    https://stackoverflow.com/questions/47148872/webdrivers-executable-may-have-wrong-permissions-please-see-https-sites-goo 
+
+from EdgeWhatsApp import EdgeWhatsApp
+
+def main():
+   print('wapp-bulk-send...')
+   
+   whatsapp = EdgeWhatsApp(r'C:\Users\Ilton\Source\Repos\wapp-bulk-send\edgedriver_win64\msedgedriver.exe')
+
+   mensagem = 'Ignora essa mensagem, to testando um bot aqui rapidão...'
+
+   whatsapp.send_msg('Grupinho da Baixada 🎩', mensagem)
+   whatsapp.send_msg('Amanda Martins', mensagem)
+   
+   del whatsapp
 
 if __name__ == "__main__":
    main()
